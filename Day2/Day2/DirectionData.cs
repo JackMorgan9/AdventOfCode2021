268127c6909102e98852a3d7dc581139db4d48e0
JackMorgan9 <jack-morgan1@hotmail.co.uk>
2021-12-03 21:51:43 +0000

    Added solution for Day1 and Day2.

---
+++ Day2/Day2/DirectionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class DirectionData
+    {
+        public string Direction { get; set; }
+
+        public int Distance { get; set; }
+    }
+}
--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day2
 {
@@ -6,17 +9,46 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines("Input/input.txt").ToList();
+
+            List<DirectionData> data = new List<DirectionData>();
+            lines.ForEach(x => data.Add(new DirectionData { Direction = x.Split(" ")[0], Distance = int.Parse(x.Split(" ")[1]) }));
+
+            Problem1(data);
+            Problem2(data);
         }
 
-        private static void Problem1()
+        private static void Problem1(List<DirectionData> data)
         {
+            int forwardTotal = data.Where(x => x.Direction == "forward").Select(x => x.Distance).Sum();
+            int upTotal = data.Where(x => x.Direction == "up").Select(x => x.Distance).Sum();
+            int downTotal = data.Where(x => x.Direction == "down").Select(x => x.Distance).Sum();
 
+            int depthTotal = downTotal - upTotal;
+
+            Console.WriteLine(depthTotal * forwardTotal);
         }
 
-        private static void Problem2()
+        private static void Problem2(List<DirectionData> data)
         {
+            int depth = 0;
+            int aim = 0;
+            int horizontal = 0;
 
+            foreach (var item in data)
+            {
+                if (item.Direction == "forward")
+                {
+                    horizontal += item.Distance;
+                    depth = depth + (aim * item.Distance);
+                }
+                else if (item.Direction == "up")
+                    aim -= item.Distance;
+                else
+                    aim += item.Distance;
+            }
+
+            Console.WriteLine(depth * horizontal);
         }
     }
 }
