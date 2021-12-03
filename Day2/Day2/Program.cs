1b67d29cdf194c4996366bf82a2f6c3b32656cd4
JackMorgan9 <jack-morgan1@hotmail.co.uk>
2021-12-03 20:33:50 +0000

    Initial commit of Day2

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void Problem1()
+        {
+
+        }
+
+        private static void Problem2()
+        {
+
+        }
+    }
+}
